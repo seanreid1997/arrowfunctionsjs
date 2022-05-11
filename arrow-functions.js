6db474592f2c595c8f0ad19fdb014f1521f3f8d2
seanreid1997 <reid.sean14@yahoo.com>
2022-05-11 18:02:34 +0000

    Initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,40 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a,b) {
+    return a + b;
+}
+
+let sum = addTwoNumbers(3,5);
+console.log(sum)
 
 // Arrow Function With Parameters
 
+const addTwoNumbersii = (a,b) => {
+    return a + b;
+}
+
+let sum2 = addTwoNumbersii(3,5);
+console.log(sum2)
+
 // Single Line Arrow Function With Parameters
+const addTwoNumbersiii = (a,b) => a + b;
+
+let sum3 = addTwoNumbersiii(3,5);
+console.log(sum3)
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello World!')
+
+const sayHello = () => console.log('Hello');
+sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string
+    </p>`
+)
+
+console.log(returnMultipleLines())
